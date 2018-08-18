c702525acfa2ebb0ba61837fb820a9440d98fc14
Bjorn Tillenius <bjorn@tillenius.me>
2018-08-18 11:20:26 +0200

    Add a simple reactstrap  page.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+    Collapse,
+    Media,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container,
+    Row,
+    Col,
+    Jumbotron,
+    Button
+} from 'reactstrap';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar light expand="md" className="d-flex justify-content-center">
+                    <NavbarBrand href="/">
+                      <Media object src="/jurge-website-header.577x130.png">
+                      </Media>
+                    </NavbarBrand>
+                </Navbar>
+                <Navbar dark className="bg-dark" expand="md">
+                  <Nav>
+                    <NavItem>
+                      <NavLink href="#">Örhängen</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink href="#">Ringar</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink href="#">Armband</NavLink>
+                    </NavItem>
+                  </Nav>
+                </Navbar>
+            </div>
+        );
+    }
 }
 
 export default App;
